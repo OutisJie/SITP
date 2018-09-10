6c9a2ab7e2bf018e1732528d6f06bcf776f3ec61
OutisJie <jie1.wu@sumscope.com>
2018-09-10 17:58:29 +0800

    update react

--- explorer-front/src/App.js
+++ explorer-front/src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import { Router, BrowserRouter, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Page = ({ title }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
+    </header>
+    <p className="App-intro">
+      this is the {title} page.
+    </p>
+    <p>
+      <Link to="/">Home</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
+    <p>
+      <Link to="/settings">Settings</Link>
+    </p>
+  </div>
+);
+
+const Home = (props) => (
+  <Page title="Home"/>
+);
+
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/setting" component={Settings}/>
+      </Router>
     );
   }
 }
